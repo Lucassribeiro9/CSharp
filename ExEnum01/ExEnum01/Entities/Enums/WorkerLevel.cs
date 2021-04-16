f3bd82847bbddf20271e0d1d159626caa5f7276a
lucas.devlop94@gmail.com <lucas.devlop94@gmail.com>
2021-04-15 21:14:15 -0300

    exercicio com composicao

---
+++ ExEnum01/ExEnum01/Entities/Enums/WorkerLevel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExEnum01.Entities.Enums
+{
+    enum WorkerLevel : int
+    {
+        Junior = 0,
+        MidLevel =  1,
+        Senior = 2
+    }
+}
