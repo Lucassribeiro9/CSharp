7281decbe81a9ba1c9b41a5422756dd478dfdfad
lucas.devlop94@gmail.com <lucas.devlop94@gmail.com>
2021-04-17 20:53:39 -0300

    StringBuilder

---
+++ DemoStringBuilder/DemoStringBuilder/Entities/Comment.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DemoStringBuilder.Entities
+{
+    class Comment
+    {
+        public string Name { get; set; }
+
+        public Comment()
+        {
+
+        }
+
+        public Comment(string name)
+        {
+            Name = name;
+        }
+    }
+
+}
---
+++ DemoStringBuilder/DemoStringBuilder/Program.cs
@@ -0,0 +1,19 @@
+﻿using DemoStringBuilder.Entities;
+using System;
+
+namespace DemoStringBuilder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Comment c1 = new Comment("Have a nice trip");
+            Comment c2 = new Comment("Wow that's awesome");
+            Post p1 = new Post(
+                DateTime.Parse("21/06/2018 13:05:44"),
+                "Traveling to New Zeland",
+                "I'm going to visit this wonderful country",
+                12);
+        }
+    }
+}
