5abda3b014d5d10f931ca7f53daf713f1ef5d700
Lucassribeiro9 <lucas.devlop94@gmail.com>
2021-10-22 21:12:47 -0300

    Refazendo composicoes e enumeracoes

---
+++ NewExOrderItem/NewExOrderItem/Entities/Order.cs
@@ -0,0 +1,58 @@
+﻿using NewExOrderItem.Entities.Enums;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace NewExOrderItem.Entities
+{
+    class Order
+    {
+        public DateTime Moment { get; set; }
+        public OrderStatus Status { get; set; }
+        public Client Client { get; set; }
+        public List<OrderItem> Items { get; set; } = new List<OrderItem>();
+
+        public Order() { }
+        public Order(DateTime moment, OrderStatus status, Client client)
+        {
+            Moment = moment;
+            Status = status;
+            Client = client;
+            
+        }
+
+        public void AddItems(OrderItem item)
+        {
+            Items.Add(item);
+        }
+        public void RemoveItems(OrderItem item)
+        {
+            Items.Remove(item);
+        }
+
+        public double Total()
+        {
+            double sum = 0;
+            foreach (OrderItem item in Items)
+            {
+                sum += item.SubTotal();
+            }
+            return sum;
+        }
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("Order moment: " + Moment.ToString("dd/MM/yyyy HH:mm:ss"));
+            sb.AppendLine("Order status: " + Status);
+            sb.AppendLine("Client: " + Client);
+            sb.AppendLine("Order items:");
+            foreach (OrderItem item in Items)
+            {
+                sb.AppendLine(item.ToString());
+            }
+            sb.AppendLine("Total price: $" + Total().ToString("F2", CultureInfo.InvariantCulture));
+            return sb.ToString();
+        }
+    }
+}
---
+++ NewExOrderItem/NewExOrderItem/Program.cs
@@ -0,0 +1,44 @@
+﻿using NewExOrderItem.Entities;
+using NewExOrderItem.Entities.Enums;
+using System;
+using System.Globalization;
+
+namespace NewExOrderItem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the client data:");
+            Console.Write("Name: ");
+            string clientName = Console.ReadLine();
+            Console.Write("Email: ");
+            string email = Console.ReadLine();
+            Console.Write("Birth date (DD/MM/YYYY): ");
+            DateTime birthDate = DateTime.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the order data: ");
+            Console.Write("Status: ");
+            OrderStatus status = Enum.Parse<OrderStatus>(Console.ReadLine());
+            Client client = new Client(clientName, email, birthDate);
+            Order order = new Order(DateTime.Now, status, client);
+            Console.Write("How many items to order? ");
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"Enter #{i} item data: ");
+                Console.Write("Product name: ");
+                string nameProduct = Console.ReadLine();
+                Console.Write("Product price: ");
+                double priceProduct = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Product product = new Product(nameProduct, priceProduct);
+                Console.Write("Quantity: ");
+                int quantity = int.Parse(Console.ReadLine());
+                OrderItem orderItem = new OrderItem(quantity, priceProduct, product);
+                order.AddItems(orderItem);
+            }
+            Console.WriteLine();
+            Console.WriteLine("ORDER SUMMARY:");
+            Console.WriteLine(order);
+        }
+    }
+}
