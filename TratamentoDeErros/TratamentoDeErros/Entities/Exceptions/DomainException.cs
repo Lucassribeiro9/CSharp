2ebc864d6b6b63b8bfaeb475795d7d560d173f43
Lucassribeiro9 <lucas.devlop94@gmail.com>
2021-11-08 20:18:00 -0300

    Exemplo de tratamento de erros

---
+++ TratamentoDeErros/TratamentoDeErros/Entities/Exceptions/DomainException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TratamentoDeErros.Entities.Exceptions
+{
+    class DomainException : ApplicationException
+    {
+        public DomainException(string message) : base(message) { }
+
+    }
+}
---
+++ TratamentoDeErros/TratamentoDeErros/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Globalization;
+using TratamentoDeErros.Entities;
+using TratamentoDeErros.Entities.Exceptions;
+
+namespace TratamentoDeErros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+            Console.Write("Room number: ");
+            int room = int.Parse(Console.ReadLine());
+            Console.Write("Check-in date: ");
+            DateTime check_in = DateTime.Parse(Console.ReadLine());
+            Console.Write("Check-out date: ");
+            DateTime check_out = DateTime.Parse(Console.ReadLine());
+
+           // tratamento de erros - jeito porco
+           /* if (check_in <= check_out)
+            {
+                Console.WriteLine("Check-out date must be after check-in date");
+            }
+            else
+            {
+                Reservation reservation = new Reservation(room, check_in, check_out);
+                Console.WriteLine(reservation);
+             Console.WriteLine();
+            Console.WriteLine("Enter data to update the reservation: ");
+            Console.Write("Room number: ");
+            room = int.Parse(Console.ReadLine());
+            Console.Write("Check-in date: ");
+            check_in = DateTime.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Check-out date: ");
+            check_out = DateTime.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                DateTime now = DateTime.Now;
+                if (check_in < now || check_out < now)
+                {
+                    Console.WriteLine("Error in reservation: Reservation dates for update must be future dates");
+                }
+                else if (check_in <= check_out)
+                {
+                    Console.WriteLine("Check-out date must be after check-in date");
+
+                }
+                else
+                {
+                    reservation.UpdateDates(check_in, check_out);
+                }
+            }
+            */
+            Reservation reservation = new Reservation(room, check_in, check_out);
+            Console.WriteLine(reservation);
+            Console.WriteLine();
+            Console.WriteLine("Enter data to update the reservation: ");
+            Console.Write("Room number: ");
+            room = int.Parse(Console.ReadLine());
+            Console.Write("Check-in date: ");
+            check_in = DateTime.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Check-out date: ");
+            check_out = DateTime.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            reservation.UpdateDates(check_in, check_out);
+            Console.WriteLine();
+            Console.WriteLine("Update reservation: ");
+            Console.WriteLine($"Reservation: {reservation}");
+
+            }
+            catch (DomainException err) // erros da regra de negócio
+            {
+
+                Console.WriteLine($"Error in reservation: {err.Message}");
+            }
+            catch (FormatException err) // erro de formato (string no lugar de double e assim vai)
+            {
+                Console.WriteLine($"Format error: {err.Message}");
+            }
+            catch(Exception err)
+            {
+                Console.WriteLine($"Erro inesperado: {err.Message}");
+            }   
+        }
+    }
+}
