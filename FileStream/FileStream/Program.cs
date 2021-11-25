3d792606e926647e3b88a934ea76998bbcab6508
Lucassribeiro9 <lucas.devlop94@gmail.com>
2021-11-25 15:19:08 -0300

    FileStream e StreamReader

---
+++ FileStream/FileStream/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace FileStreamfs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\ESD\teste.txt";
+            StreamReader sr = null;
+            try
+            {
+                sr = File.OpenText(path);
+                while (!sr.EndOfStream) // para ler todas as linhas do arquivo
+                {
+                    string line = sr.ReadLine();
+                    Console.WriteLine(line);
+                }
+               
+            }
+            catch (IOException err)
+            {
+                Console.WriteLine($"Ocorreu um erro! Erro: {err.Message}");
+
+            }
+            finally
+            {
+                if (sr != null) sr.Close();
+            }
+        }
+    }
+}
---
+++ Jogo-de-Xadrez/Jogo-de-Xadrez/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Jogo_de_Xadrez
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
