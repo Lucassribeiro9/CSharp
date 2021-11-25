300b4a84aaf6ff8e923f30727b7838335a9c4f71
Lucassribeiro9 <lucas.devlop94@gmail.com>
2021-11-25 16:38:56 -0300

    StreamWriter

--- Streamwriter/Streamwriter/Program.cs
+++ Streamwriter/Streamwriter/Program.cs
@@ -1,30 +1,29 @@
 ﻿using System;
 using System.IO;
 
-namespace UsingBlock
+namespace Streamwriter
 {
     class Program
     {
         static void Main(string[] args)
         {
-            string path = @"C:\ESD\teste.txt";
+            string sourcePath = @"C:\ESD\teste.txt";
+            string targetPath = @"C:\ESD\teste3.txt";
             try
             {
-                using (StreamReader sr = File.OpenText(path))
+                string[] lines = File.ReadAllLines(sourcePath);
+                using (StreamWriter sw = File.AppendText(targetPath))
                 {
-                    while (!sr.EndOfStream)
+                    foreach (string line in lines)
                     {
-                        string line = sr.ReadLine();
-                        Console.WriteLine(line);
+                        sw.WriteLine(line.ToUpper());
                     }
                 }
-
             }
             catch (IOException err)
             {
                 Console.WriteLine($"Ocorreu um erro. Erro: {err.Message}");
             }
-
         }
     }
 }
