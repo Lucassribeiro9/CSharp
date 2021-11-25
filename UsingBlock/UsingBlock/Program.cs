79aabd8edde14fd653062737ecafe0930807e9db
Lucassribeiro9 <lucas.devlop94@gmail.com>
2021-11-25 15:39:51 -0300

    using block

---
+++ UsingBlock/UsingBlock/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace UsingBlock
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\ESD\teste.txt";
+            try
+            {
+
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string line = sr.ReadLine();
+                        Console.WriteLine(line);
+                    }
+                }
+
+            }
+            catch (IOException err)
+            {
+                Console.WriteLine($"Ocorreu um erro. Erro: {err.Message}");
+            }
+
+        }
+    }
+}
