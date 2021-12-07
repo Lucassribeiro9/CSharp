d242c408b5f31a88698d776789730584619510aa
Lucassribeiro9 <lucas.devlop94@gmail.com>
2021-12-06 21:28:22 -0300

    Directory

---
+++ DirInfo/DirInfo/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace DirInfo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\ESD";
+            try
+            {
+                // lista as pastas
+                var folders = Directory.EnumerateDirectories(path, "*.*", SearchOption.AllDirectories);
+                Console.WriteLine("Folders"); 
+                foreach (string s in folders)
+                {
+                    Console.WriteLine(s);
+                }
+                // lista os arquivos
+                var files = Directory.EnumerateFiles(path, "*.*", SearchOption.AllDirectories);
+                foreach (string t in files) 
+                {
+                    Console.WriteLine(t);
+                }
+                // cria a pasta
+                Directory.CreateDirectory(path + @"\Nova"); 
+            }
+            catch (IOException err)
+            {
+                Console.WriteLine("An error occurred!");
+                Console.WriteLine(err.Message);
+            }
+        }
+    }
+}
