293b91fe1a48c1e42c3f1af2d6420ce48321dd1a
Lucassribeiro9 <lucas.devlop94@gmail.com>
2021-12-06 21:46:36 -0300

    Path

---
+++ PathProj/PathProj/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+
+namespace PathProj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\ESD\teste.txt";
+            Console.WriteLine(Path.DirectorySeparatorChar); // caractere de separação
+            Console.WriteLine($"Get Directory name: {Path.GetDirectoryName(path)}"); // nome do diretório
+            Console.WriteLine("Path separator: " + Path.PathSeparator); // separador
+            Console.WriteLine(Path.GetFileName(path)); // nome do arquivo
+            Console.WriteLine(Path.GetFileNameWithoutExtension(path)); // sem extensão
+            Console.WriteLine(Path.GetExtension(path)); // extensão
+            Console.WriteLine(Path.GetFullPath(path));
+            Console.WriteLine(Path.GetTempPath());
+        }
+    }
+}
