1b377486c799e5458c0b050ea1520a3c30dfc3cd
Lucassribeiro9 <lucas.devlop94@gmail.com>
2022-02-22 12:54:27 -0300

    Finalizando algoritmo sem interface

---
+++ ExIntroInterface/ExIntroInterface/Entities/Vehicle.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExIntroInterface.Entities
+{
+    class Vehicle
+    {
+        public string Model { get; set; }
+
+        public Vehicle(string model)
+        {
+            Model = model;
+        }
+    }
+}
--- ExIntroInterface/ExIntroInterface/Program.cs
+++ ExIntroInterface/ExIntroInterface/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using ExIntroInterface.Entities;
+using ExIntroInterface.Services;
+using System;
+using System.Globalization;
 
 namespace ExIntroInterface
 {
@@ -6,7 +9,24 @@ namespace ExIntroInterface
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter rental data");
+            Console.Write("Car model: ");
+            string model = Console.ReadLine();
+            Console.Write("Pickup (dd/mm/yyyy hh: mm): ");
+            DateTime start = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture);
+            Console.Write("Return (dd/mm/yyyy hh: mm): ");
+            DateTime finish = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture);
+            Console.Write("Enter price per hour: ");
+            double priceHr = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Enter price per day: ");
+            double priceDay = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            CarRental carRental = new CarRental(start, finish, new Vehicle(model));
+            RentalService rentalService = new RentalService(priceHr, priceDay);
+            rentalService.ProcessInvoice(carRental);
+
+            Console.WriteLine("Invoice: ");
+            Console.WriteLine(carRental.Invoice);
         }
     }
 }
