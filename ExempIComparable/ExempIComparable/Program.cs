7f4bca92a4ffde26aa38c51549879742a4e32d85
Lucassribeiro9 <lucas.devlop94@gmail.com>
2022-02-28 21:16:44 -0300

    Exemplo implementando interface IComparable

---
+++ ExempIComparable/ExempIComparable/Program.cs
@@ -0,0 +1,36 @@
+﻿using ExempIComparable.Entities;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace ExempIComparable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\ESD\teste.txt";
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    List<Employee> list = new List<Employee>();
+                    while (!sr.EndOfStream)
+                    {
+                        list.Add(new Employee(sr.ReadLine()));
+                    }
+                    list.Sort();
+                    foreach (Employee emp in list)
+                    {
+                        Console.WriteLine(emp);
+                    }
+                }
+            }
+            catch(IOException msg)
+            {
+                Console.WriteLine("Error: " + msg);
+            }
+            
+        }
+    }
+}
