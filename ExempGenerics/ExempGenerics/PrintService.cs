1a1a3000594ba2d1b07ee988a4aba5852691b8e3
Lucassribeiro9 <lucas.devlop94@gmail.com>
2022-03-01 13:13:56 -0300

    Implementado e parametrizado o type safety

--- ExempGenerics/ExempGenerics/PrintService.cs
+++ ExempGenerics/ExempGenerics/PrintService.cs
@@ -4,12 +4,12 @@ using System.Text;
 
 namespace ExempGenerics
 {
-    class PrintService
+    class PrintService<T> // pode ser qualquer letra para o tipo genérico
     {
-        private int[] _values = new int[10];
+        private T[] _values = new T[10];
         private int _count = 0;
 
-        public void AddValue(int value)
+        public void AddValue(T value)
         {
             if (_count == 10)
             {
@@ -18,7 +18,7 @@ namespace ExempGenerics
             _values[_count] = value;
             _count++;
         }
-        public int First()
+        public T First()
         {
             if (_count == 0)
             {
--- ExempGenerics/ExempGenerics/Program.cs
+++ ExempGenerics/ExempGenerics/Program.cs
@@ -6,12 +6,12 @@ namespace ExempGenerics
     {
         static void Main(string[] args)
         {
-            PrintService printService = new PrintService();
+            PrintService<string> printService = new PrintService<string>(); // na hora de instanciar nós definimos o tipo
             Console.Write("How many values: ");
             int n = int.Parse(Console.ReadLine());
             for (int i = 0; i < n; i++)
             {
-                int x = int.Parse(Console.ReadLine());
+                string x = Console.ReadLine();
                 printService.AddValue(x);
             }
             printService.Print();
