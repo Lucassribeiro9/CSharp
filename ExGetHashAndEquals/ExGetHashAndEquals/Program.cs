861dd840bdbfd8ffd84289d5110943f3ab31d7da
Lucassribeiro9 <lucas.devlop94@gmail.com>
2022-03-04 11:51:41 -0300

    Testes com Equals e GetHashCode

---
+++ ExGetHashAndEquals/ExGetHashAndEquals/Program.cs
@@ -0,0 +1,19 @@
+﻿using ExGetHashAndEquals.Entities;
+using System;
+
+namespace ExGetHashAndEquals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Client a = new Client { Name = "Maria", Email = "maria@gmail" };
+            Client b = new Client { Name = "Alex", Email = "alex@gmail" };
+
+            Console.WriteLine(a.Equals(b));
+            Console.WriteLine(a.GetHashCode());
+            Console.WriteLine(b.GetHashCode());
+            Console.WriteLine(a == b);
+        }
+    }
+}
