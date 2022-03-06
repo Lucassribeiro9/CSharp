13360a012b89fe48ffcd70963a751ac75912dfa5
Lucassribeiro9 <lucas.devlop94@gmail.com>
2022-03-05 21:16:04 -0300

    Primeira demo com conjuntos e uso do hashset

---
+++ ExHashAndSortedSet/ExHashAndSortedSet/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ExHashAndSortedSet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> set = new HashSet<string>();
+            set.Add("TV");
+            set.Add("Notebook");
+            set.Add("Tablet");
+            Console.WriteLine(set.Contains("Notebook"));
+
+            foreach (string obj in set) // mostrar os itens do set
+            {
+                Console.WriteLine(obj);
+            }
+        }
+    }
+}
