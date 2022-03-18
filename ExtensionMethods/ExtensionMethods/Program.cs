638f53826e214580b604d303f39d3834cb743e93
Lucassribeiro9 <lucas.devlop94@gmail.com>
2022-03-18 13:54:39 -0300

    Adiçao do exemplo 2

--- ExtensionMethods/ExtensionMethods/Program.cs
+++ ExtensionMethods/ExtensionMethods/Program.cs
@@ -8,6 +8,9 @@ namespace ExtensionMethods
         {
             DateTime dt = new DateTime(2018, 11, 16, 8, 10, 45);
             Console.WriteLine(dt.ElapsedTime());
+
+           String s1 = "Good morning dear students!";
+            Console.WriteLine(s1.Cut(10));
         }
     }
 }
---
+++ ExtensionMethods/ExtensionMethods/StringEx.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExtensionMethods
+{
+    static class StringEx
+    {
+        public static string Cut(this string thisObj, int count)
+        {
+            if (thisObj.Length <= count)
+            {
+                return thisObj;
+            }
+            else
+            {
+                return thisObj.Substring(0, count) + "...";
+            }
+        }
+    }
+}
