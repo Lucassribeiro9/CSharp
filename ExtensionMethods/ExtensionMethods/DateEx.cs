79ac86b551453ae5f4a7946c158933773be664f9
Lucassribeiro9 <lucas.devlop94@gmail.com>
2022-03-18 13:39:05 -0300

    Exemplo 1 do uso de extension methods

---
+++ ExtensionMethods/ExtensionMethods/DateEx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+
+namespace ExtensionMethods
+{
+    static class DateEx
+    {
+        public static string ElapsedTime(this DateTime thisObj)
+        {
+            TimeSpan duration = DateTime.Now.Subtract(thisObj);
+            if (duration.TotalHours < 24)
+            {
+                return duration.TotalHours.ToString("F1", CultureInfo.InvariantCulture) + " hours";
+            }
+            else
+            {
+                return duration.TotalDays.ToString("F1", CultureInfo.InvariantCulture) + " days";
+
+            }
+        }
+    }
+}
---
+++ ExtensionMethods/ExtensionMethods/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ExtensionMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dt = new DateTime(2018, 11, 16, 8, 10, 45);
+            Console.WriteLine(dt.ElapsedTime());
+        }
+    }
+}
+
