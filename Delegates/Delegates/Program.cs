ff648d8727c069bbc09018ca0d9a07ab5e7bd3e7
Lucassribeiro9 <lucas.devlop94@gmail.com>
2022-03-22 11:26:27 -0300

    implementação simples do delegate

---
+++ Delegates/Delegates/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Delegates.Services;
+
+namespace Delegates
+{
+    class Program
+    {
+        delegate double BinaryNumericOperation(double n1, double n2); // referencia da função
+        static void Main(string[] args)
+        {
+            double a = 10;
+            double b = 12;
+
+            BinaryNumericOperation op = CalculationServices.Sum;
+
+            double result = op(a, b);
+            Console.WriteLine(result);
+        }
+    }
+}
