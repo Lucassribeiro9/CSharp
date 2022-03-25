10cc80c72ef4ec83fd7b30b6ce9a19c530a4283e
Lucassribeiro9 <lucas.devlop94@gmail.com>
2022-03-25 15:41:23 -0300

    uso da expressão lambda

--- ExPredicate/ExPredicate/Program.cs
+++ ExPredicate/ExPredicate/Program.cs
@@ -16,7 +16,7 @@ namespace ExPredicate
             list.Add(new Product("Tablet", 350.50));
             list.Add(new Product("HD Case", 80.90));
 
-            Func<Product, string> func = NameUpper;
+            Func<Product, string> func = p => p.Name.ToUpper();
             
             List<string> result = list.Select(func).ToList();
             foreach (string s in result)
@@ -24,9 +24,6 @@ namespace ExPredicate
                 Console.WriteLine(s);
             }
         }
-        static string NameUpper(Product p)
-        {
-            return p.Name.ToUpper();
-        }
+       
     }
 }
