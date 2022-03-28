78613b362ff28b4a56d790b3d216f22fd6fdf582
Lucassribeiro9 <lucas.devlop94@gmail.com>
2022-03-28 17:06:34 -0300

    implementação do LINQ e criação de objeto anônimo

---
+++ ExLINQ/ExLINQ/Entities/Category.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExLINQ.Entities
+{
+    class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Tier { get; set; }
+
+        public Category(int id, string name, int tier)
+        {
+            Id = id;
+            Name = name;
+            Tier = tier;
+        }
+
+        public Category()
+        {
+        }
+    }
+}
--- ExLINQ/ExLINQ/Program.cs
+++ ExLINQ/ExLINQ/Program.cs
@@ -1,24 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using ExLINQ.Entities;
 
 namespace ExLINQ
 {
     class Program
     {
+        static void Print<T>(string message, IEnumerable<T> collection)
+        {
+            Console.WriteLine(message);
+            foreach (T obj in collection)
+            {
+                Console.WriteLine(obj);
+            }
+            Console.WriteLine();
+        }
         static void Main(string[] args)
         {
-            // Specify the data source.
-            int[] numbers = new int[] { 2, 3, 4, 5 };
+            Category c1 = new Category() { Id = 1, Name = "Tools", Tier = 2 };
+            Category c2 = new Category() { Id = 2, Name = "PC", Tier = 1 };
+            Category c3 = new Category() { Id = 3, Name = "Eletronics", Tier = 1 };
 
-            // Define the query expression.
-            IEnumerable<int> result = numbers.Where(x => x % 2 == 0).Select(x => 10 * x);
-
-            // Execute the query.
-            foreach (int x in result)
+            List<Product> products = new List<Product>()
             {
-                Console.WriteLine(x);
-            }
+                new Product(){Id = 1, Name = "PC", Price = 2000, Category = c2 },
+                new Product(){Id = 2, Name = "Hammer", Price = 90, Category = c1 },
+                new Product(){Id = 1, Name = "TV", Price = 575, Category = c3 },
+                new Product(){Id = 1, Name = "Notebook", Price = 4999, Category = c2 }
+            };
+
+            var r1 = products.Where(p => p.Category.Tier == 1 && p.Price < 900);
+            Print($"Tier 1 and Price under $900: ", r1);
+
+            var r2 = products.Where(p => p.Category.Name == "Tools").Select(p => p.Name);
+            Print("Names of products from tools: ", r2);
+
+            var r3 = products.Where(p => p.Name[0] == 'C').Select(p => new { p.Name, p.Price, CategoryName = p.Category.Name });
+            Print("Names started with 'C' and anonymous object: ", r3);
+
         }
     }
 }
