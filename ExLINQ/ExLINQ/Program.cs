f81b34439081c5ca0152c7079e549fdcd5940448
Lucassribeiro9 <lucas.devlop94@gmail.com>
2022-03-28 15:17:21 -0300

    intro ao LINQ

---
+++ ExLINQ/ExLINQ/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ExLINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Specify the data source.
+            int[] numbers = new int[] { 2, 3, 4, 5 };
+
+            // Define the query expression.
+            IEnumerable<int> result = numbers.Where(x => x % 2 == 0).Select(x => 10 * x);
+
+            // Execute the query.
+            foreach (int x in result)
+            {
+                Console.WriteLine(x);
+            }
+        }
+    }
+}
