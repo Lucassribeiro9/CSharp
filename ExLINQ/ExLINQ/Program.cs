aa1cfef4ce2597ec9d74a668774ae933395da57b
Lucassribeiro9 <lucas.devlop94@gmail.com>
2022-03-29 17:20:50 -0300

    implementações de classe e uso da expressão lambda

--- ExLINQ/ExLINQ/Program.cs
+++ ExLINQ/ExLINQ/Program.cs
@@ -39,6 +39,49 @@ namespace ExLINQ
             var r3 = products.Where(p => p.Name[0] == 'C').Select(p => new { p.Name, p.Price, CategoryName = p.Category.Name });
             Print("Names started with 'C' and anonymous object: ", r3);
 
+            var r4 = products.Where(p => p.Category.Tier == 1).OrderBy(p => p.Price).ThenBy(p => p.Name);
+            Print("Tier 1 order by price and name: ", r4);
+
+            var r5 = r4.Skip(2).Take(4);
+            Print("Tier 1 order by price and name (skip & take): ", r5);
+
+            var r6 = products.First();
+            Console.WriteLine("First test 1: " + r6);
+
+            var r7 = products.Where(p => p.Price > 3000).FirstOrDefault();
+            Console.WriteLine("First test 2: " + r7);
+
+            var r8 = products.Where(p => p.Id == 3).SingleOrDefault();
+            Console.WriteLine("Single or default: " + r8);
+
+            var r9 = products.Max(p => p.Price);
+            Console.WriteLine("Max price: " + r9);
+
+            var r10 = products.Min(p => p.Price);
+            Console.WriteLine("Min price: " + r10);
+
+            var r11 = products.Where(p => p.Category.Id == 1).Sum(p => p.Price);
+            Console.WriteLine("Category 1 Sum prices: " + r11);
+
+            var r12 = products.Where(p => p.Category.Id == 1).Average(p => p.Price);
+            Console.WriteLine("Category 1 Average prices: " + r12);
+
+            var r13 = products.Where(p => p.Category.Id == 5).Select(p => p.Price).DefaultIfEmpty(0.0).Average();
+            Console.WriteLine("Category 5 Average prices: " + r13);
+
+            var r14 = products.Where(p => p.Category.Id == 1).Select(p => p.Price).Aggregate(0.0, (x, y) => x + y);
+            Console.WriteLine("Category 1 aggregate sum: " + r14);
+
+            var r15 = products.GroupBy(p => p.Category);
+            foreach (IGrouping<Category, Product> group in r15)
+            {
+                Console.WriteLine($"Category: {group.Key.Name}");
+                foreach (Product p in group)
+                {
+                    Console.WriteLine(p);
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
