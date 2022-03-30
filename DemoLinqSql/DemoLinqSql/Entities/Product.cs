3999ca3b605e330812c693d0b30994523bcdaee0
Lucassribeiro9 <lucas.devlop94@gmail.com>
2022-03-30 14:15:20 -0300

    demo uso do linq com streamreader

---
+++ DemoLinqSql/DemoLinqSql/Entities/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DemoLinqSql.Entities
+{
+    class Product
+    {
+        
+        public double Price { get; set; }
+        public string Name { get; set; }
+
+        public Product(double price, string name)
+        {
+            Price = price;
+            Name = name;
+        }
+    }
+}
---
+++ DemoLinqSql/DemoLinqSql/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using DemoLinqSql.Entities;
+using System.IO;
+using System.Globalization;
+using System.Linq;
+
+namespace DemoLinqSql
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter full file path: ");
+            string path = Console.ReadLine();
+
+            List<Product> list = new List<Product>();
+            using (StreamReader sr = File.OpenText(path))
+            {
+                while (!sr.EndOfStream)
+                {
+                    string[] fields = sr.ReadLine().Split(',');
+                    string name = fields[0];
+                    double price = double.Parse(fields[1], CultureInfo.InvariantCulture);
+                    list.Add(new Product(price, name));
+                }
+            }
+            var avg = list.Select(p => p.Price).DefaultIfEmpty(0).Average();
+            Console.WriteLine($"Average price: {avg.ToString("F2", CultureInfo.InvariantCulture)}");
+
+            var names = list.Where(p => p.Price < avg).OrderByDescending(p => p.Name).Select(p => p.Name);
+            foreach (string name in names)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
