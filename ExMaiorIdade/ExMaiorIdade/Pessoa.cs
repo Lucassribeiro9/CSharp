55eb2fba2ae73df2a8366813b2591dc2e1ca23e6
Lucassribeiro9 <lucas.devlop94@gmail.com>
2022-06-10 12:44:30 -0300

    voltando a praticar

---
+++ ExMaiorIdade/ExMaiorIdade/Pessoa.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExMaiorIdade
+{
+    class Pessoa
+    {
+        public int Idade { get; set; }
+        public string Nome { get; set; }
+
+        public Pessoa()
+        {
+        }
+
+        public Pessoa(int idade, string nome)
+        {
+            Idade = idade;
+            Nome = nome;
+        }
+        public override string ToString()
+        {
+            return "Nome: " + Nome + ", Idade: " + Idade;
+        }
+    }
+
+
+}
---
+++ ExMaiorIdade/ExMaiorIdade/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ExMaiorIdade
+{
+    class Program
+    {
+        static void Print<T>(string message, IEnumerable<T> collection)
+        {
+            Console.WriteLine(message);
+            foreach (T obj in collection)
+            {
+                Console.WriteLine(obj);
+            }
+            Console.WriteLine();
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Quantas pessoas deseja cadastrar: ");
+            int n = int.Parse(Console.ReadLine());
+            List<Pessoa> list = new List<Pessoa>();
+            for (int i = 1; i <= n; i++)
+            {
+                Console.Write("Digite o nome: ");
+                string nome = Console.ReadLine();
+                Console.Write("Idade: ");
+                int idade = int.Parse(Console.ReadLine());
+                list.Add(new Pessoa(idade, nome));
+
+            }
+            foreach (Pessoa obj in list)
+            {
+                Console.WriteLine(obj);
+            }
+            
+            // to batendo cabeça pra trazer uma lista de idade por ordem crescente acima
+        }
+    }
+}
