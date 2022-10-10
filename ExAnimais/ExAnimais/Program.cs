8127c98b10b6df6f89095e003d3ea3b39fe2591c
Lucassribeiro9 <lucas.devlop94@gmail.com>
2022-10-10 14:36:25 -0300

    criando validadores

--- ExAnimais/ExAnimais/Program.cs
+++ ExAnimais/ExAnimais/Program.cs
@@ -1,5 +1,12 @@
 ﻿using System;
 
+/*Crie um algoritmo/programa no qual o usuário deverá informar o nome e o tipo de cinco animais de estimação. O programa deverá exibir na tela para o usuário quantos Cachorros, Gatos e peixes foram informados.
+
+Regras que deverão ser seguidas para a implementação do algoritmo:
+
+Os únicos tipos de animais válidos são Cachorro, Gato, Peixe.
+Caso seja informado um tipo diferente o programa deverá definir o tipo do animal como Peixe.
+É obrigatório criar uma classe para representar o Animal. */
 namespace ExAnimais
 {
     class Program
